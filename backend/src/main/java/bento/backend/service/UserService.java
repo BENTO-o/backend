eee163839c68bf75b60e17d0f0a15a718d3f8b7f
Lee Yeseul <yes0112@naver.com>
2024-10-13 00:29:33 +0900

    ✨: UserService 주석 해제

--- backend/src/main/java/bento/backend/service/UserService.java
+++ backend/src/main/java/bento/backend/service/UserService.java
@@ -10,8 +10,8 @@ public class UserService {
 	private final UserRepository userRepository;
 
 	public String getTest() {
-		// User user = userRepository.findById(1L)
-		// 	.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+		User user = userRepository.findById(1L)
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
 		return "test";
 	}
 }
