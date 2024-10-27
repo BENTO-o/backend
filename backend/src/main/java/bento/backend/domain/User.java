76f6e39e8677950d2c6b65f76e42110d0481d110
leesiha <mlice1030@soongsil.ac.kr>
2024-10-28 05:22:54 +0900

    :hammer:: Implement password hashing and validation in User entity

--- backend/src/main/java/bento/backend/domain/User.java
+++ backend/src/main/java/bento/backend/domain/User.java
@@ -1,7 +1,12 @@
 package bento.backend.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Entity
 @Getter
@@ -12,12 +17,17 @@ public class User {
 	@Column(name = "user_id")
 	private Long userId;
 
+	@NotBlank(message = "Username is mandatory")
+	@Size(min = 4, max = 20, message = "Username should be between 4 and 20 characters")
 	@Column(name = "username", nullable = false)
 	private String username;
 
+	@NotBlank(message = "Password is mandatory")
+	@Size(min = 8, max = 20, message = "Password should be between 8 and 20 characters")
 	@Column(name = "password", nullable = false)
 	private String password;
 
+	@Email(message = "Email should be valid")
 	@Column(name = "email", nullable = false)
 	private String email;
 
@@ -27,4 +37,19 @@ public class User {
 
 	@Column(name="oauth_provider_id")
 	private String oauthProviderId;
+
+	public void changePassword(String password) {
+		this.password = hashPassword(password);
+	}
+
+	private String hashPassword(String rawPassword) {
+		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		return passwordEncoder.encode(rawPassword);
+	}
+
+	public boolean checkPassword(String rawPassword, String hashedPassword) {
+		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		return passwordEncoder.matches(rawPassword, hashedPassword);
+	}
+
 }
