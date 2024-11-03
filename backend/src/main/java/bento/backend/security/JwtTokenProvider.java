c4fc5f28a5d9eba46ac3de20d911ef1d3333d2fc
leesiha <mlice1030@soongsil.ac.kr>
2024-11-03 19:28:58 +0900

    :sparkles:: implement JWT

---
+++ backend/src/main/java/bento/backend/security/JwtTokenProvider.java
@@ -0,0 +1,52 @@
+package bento.backend.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenProvider {
+    private final long EXPIRATION_TIME = 1000 * 60 * 30; // 30 minutes (in milliseconds)
+    private final String secretKey;
+
+    public JwtTokenProvider(@Value("${jwt.secret-key}") String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(token)
+                    .getBody();
+            return !claims.getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
