b10f7ea85a7231848e25e05a8697609f73070ff6
leesiha <mlice1030@soongsil.ac.kr>
2024-11-03 19:53:14 +0900

    :art:: add error message exception format

---
+++ backend/src/main/java/bento/backend/constant/ErrorMessages.java
@@ -0,0 +1,9 @@
+package bento.backend.constant;
+
+public class ErrorMessages {
+    public static final String USER_NOT_FOUND = "User not found with id: ";
+    public static final String EMAIL_NOT_FOUND = "User not found with email: ";
+    public static final String USERNAME_ALREADY_EXISTS = "The username '%s' is already taken. Please choose a different username.";
+    public static final String EMAIL_ALREADY_EXISTS = "An account with the email '%s' already exists. Please use a different email address.";
+    // 필요한 다른 에러 메시지들을 추가
+}
