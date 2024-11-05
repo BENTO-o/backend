64aff6e295e3bbd8d3366bf87a7dd415859ed76e
leesiha <mlice1030@soongsil.ac.kr>
2024-11-06 04:38:25 +0900

    :hammer:: modify SecurityConfig
    - 필요한 권한 설정만 진행했습니다.
    - oauth 성공/실패시 이동할 URL을 정의하고, 해당 URL들은 직접 접근할 수 없도록 정의해두었습니다.
    - 그러나, 현재 의도한 대로 동작하지 않는 것 같아 분석이 필요할 것 같습니다.

---
+++ backend/src/main/java/bento/backend/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package bento.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // 접근 권한 설정
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/oauth2/authorization/**").permitAll()  // OAuth2 인증 요청 허용
+                        .requestMatchers("/oauth/**").authenticated()  // OAuth2 인증 성공 후 토큰 발급 엔드포인트는 인증 필요
+                        .anyRequest().permitAll()  // 그 외 모든 요청은 허용
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .defaultSuccessUrl("/oauth/login/success")  // 로그인 성공 후 토큰 반환 엔드포인트
+                        .failureUrl("/oauth/login/failure")  // 로그인 실패 시 엔드포인트
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
