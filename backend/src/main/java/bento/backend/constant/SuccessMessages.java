054256dd188d12152b89716b1bd841f4bec33d7e
leesiha <mlice1030@soongsil.ac.kr>
2024-11-12 22:28:17 +0900

    :sparkles:: modify exceptions
    - GlobalExceptionHandler를 추가했습니다.
    - Exception을 수정했습니다.(NotFoundException, BadRequestException)
    - const Messages들을 추가했습니다.

---
+++ backend/src/main/java/bento/backend/constant/SuccessMessages.java
@@ -0,0 +1,6 @@
+package bento.backend.constant;
+
+public class SuccessMessages {
+    public static final String USER_REGISTERED = "User registered successfully";
+    public static final String USER_LOGGED_IN = "Login successful";
+}
