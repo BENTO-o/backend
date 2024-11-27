24073ca981ed0288b1d6e3ce2824fb469f0c2c9e
Siha Lee <mlice1030@soongsil.ac.kr>
2024-11-28 01:14:59 +0900

    Note save bookmark an dmemo in streaming (#21)
    * :sparkles:: 노트 생성 API 기능 추가
    
    - 실시간 녹음 후 북마크/메모 포함하여 POST notes 할 수 있도록 노트 API를 수정했습니다.
    
    * :art:: 기능 합친 후 포맷 개선
    
    - 기존에 만들어진 createNote 포맷에 최대한 맞춰서 제작
    - 미사용 import문 제거
    
    * :zap:: bookmark, memo 미포함 시에도 note 생성 가능
    
    - JSON 빈 배열을 기본값으로 사용하여, null 미체크로 인한 에러 발생을 막았습니다.
    
    * :fire:: 불필요한 코드 삭제
    
    * :fire:: 불필요한 코드 삭제

--- backend/src/main/java/bento/backend/dto/request/NoteCreateRequest.java
+++ backend/src/main/java/bento/backend/dto/request/NoteCreateRequest.java
@@ -1,12 +1,10 @@
 package bento.backend.dto.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
-
 
 @Getter
 @Setter
@@ -20,4 +18,7 @@ public class NoteCreateRequest {
 
 	@NotBlank(message = "File is mandatory")
 	private MultipartFile file;
+
+	private String bookmarks; // 북마크 리스트
+	private String memos;     // 메모 리스트
 }
