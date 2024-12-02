a0c6a2e1ef12032f2289f6d033c9b9b2975301f4
Lee Yeseul <yes0112@naver.com>
2024-12-03 01:30:28 +0900

    :sparkles:: STT 결과 파싱 및 duration 설정

--- backend/src/main/java/bento/backend/service/note/NoteService.java
+++ backend/src/main/java/bento/backend/service/note/NoteService.java
@@ -24,7 +24,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -33,13 +32,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.json.simple.JSONObject;
 import reactor.core.publisher.Mono;
 
 import java.util.*;
 import java.util.stream.Collectors;
 import java.time.LocalDateTime;
-
-import org.json.simple.JSONObject;
 
 @Service
 @RequiredArgsConstructor
@@ -53,6 +51,8 @@ public class NoteService {
 
 	// 노트 생성
 	public MessageResponse createNote(User user, String filePath, NoteCreateRequest request) {
+        String language = "enko"; // default language
+
 		if (request.getFolder() == null) {
 			request.setFolder("default");
 		}
@@ -60,26 +60,29 @@ public class NoteService {
         Folder folder = folderRepository.findByFolderNameAndUser(request.getFolder(), user)
                 .orElseThrow(() -> new ResourceNotFoundException("Folder not found"));
 
-        String duration = "01:00:00"; // TOOD : AI 서버에서 받아오기
-        String language = "enko";
-
         Audio audio = Audio.builder()
                 .filePath(filePath)
-                .duration(duration)
+                .duration("00:00:00")
                 .status(AudioStatus.PROCESSING)
                 .language(language)
                 .user(user)
                 .build();
 
-		audioRepository.save(audio);
-
-        // AI 서버로 요청 보내기
 		String responseJson = callAIServer();
+
+        Map<String, Object> responseMap = convertJsonToMap(responseJson);
+
+        // 응답값 바탕으로 duration 설정
+        audio.updateDuration(responseMap.get("duration").toString());
+        audioRepository.save(audio);
+
+		Map<String, Object> contentMap = (Map<String, Object>) responseMap.get("content");
+        String jsonContent = convertMapToJsonString(contentMap);
 
 		// Note 객체 생성
 		Note note = Note.builder()
 			.title(request.getTitle())
-            .content(responseJson)
+            .content(jsonContent)
 			.folder(folder)
 			.audio(audio)
 			.user(user)
@@ -130,7 +133,7 @@ public class NoteService {
     }
 
     // AI 서버로 요청 보내기
-	public String callAIServer() {
+	private String callAIServer() {
 		String uri = "/get-json"; // STT 요청 URI
 
         String responseBody = webClient.get()
@@ -151,6 +154,28 @@ public class NoteService {
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException("Failed to decode Unicode response");
+        }
+    }
+
+    // JSON String to Map 변환
+    private Map<String, Object> convertJsonToMap(String responseBody) {
+        try {
+            return objectMapper.readValue(responseBody, new TypeReference<>() {});
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to convert JSON string to Map");
+        }
+    }
+
+    // Map to JSON String 변환
+    private String convertMapToJsonString(Map<String, Object> map) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            // 맵 데이터를 JSON 문자열로 직렬화
+            return objectMapper.writeValueAsString(map);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to convert Map to JSON string");
         }
     }
 
